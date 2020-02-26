484d83885c671cc18dc3c5abc2c7f2975cb0362b
Kovalenko Ivan <s1ckretbox@gmail.com>
2020-02-26 12:38:29 +0200

    Fix: explicit specialization in non-namespace scope
    Signed-off-by: Kovalenko Ivan <s1ckretbox@gmail.com>

--- SAV/src/Graphics/VertexBufferLayout.h
+++ SAV/src/Graphics/VertexBufferLayout.h
@@ -1,4 +1,8 @@
 #pragma once
+
+#include <vector>
+#include <cassert>
+
 #include "Renderer.h"
 
 struct VertexBufferElement
@@ -43,28 +47,7 @@ public:
 	template<typename T>
 	void Push(unsigned int count)
 	{
-		static_assert(false);
-	}
-
-	template <>
-	void Push<float>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_FLOAT, count, GL_FALSE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_FLOAT);
-	}
-
-	template <>
-	void Push<unsigned int>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_UNSIGNED_INT, count, GL_FALSE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_INT);
-	}
-
-	template <>
-	void Push<unsigned char>(unsigned int count)
-	{
-		m_Elements.push_back({ GL_UNSIGNED_BYTE, count, GL_TRUE });
-		m_Stride += count * VertexBufferElement::GetSizeOfType(GL_UNSIGNED_BYTE);
+		assert(false);
 	}
 
 	inline unsigned int GetStride() const { return m_Stride; }
